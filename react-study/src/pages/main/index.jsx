47b86a2be4ac5be69789e486d055d42b4521c3b0
qfdewfs <dwqd@fdqawesf>
2023-05-09 09:20:47 +0900

    0509-01-commit

---
+++ react-study/src/pages/main/index.jsx
@@ -0,0 +1,14 @@
+import Button from '../../components/Button';
+
+import * as S from './styled';
+function Main() {
+  return (
+    <S.Container>
+      <S.MainWrapper>
+        <div>메인 페이지</div>
+        <Button>만들기</Button>
+      </S.MainWrapper>
+    </S.Container>
+  );
+}
+export default Main;
--- react-study/src/pages/router.jsx
+++ react-study/src/pages/router.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './main';
-function Router(){
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Main />}/>
-            </Routes>
-        </BrowserRouter>
-    );
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default Router;
