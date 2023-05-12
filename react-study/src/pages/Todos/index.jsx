3e7905eb79b25b70541dfdcff49376ed80595271
qfdewfs <dwqd@fdqawesf>
2023-05-12 10:31:49 +0900

    Feat.검색기능추가

--- react-study/src/pages/Todos/index.jsx
+++ react-study/src/pages/Todos/index.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
-
+import { v4 as uuidv4 } from 'uuid';
 import * as S from './styled';
 
 import CreateItemBox from './CreatItemBox';
 import ItemList from './ItemList';
+import SearchInput from '../../components/searchInput';
 
 function Todos() {
   const [TodoName, setTodoName] = useState('');
@@ -11,12 +12,19 @@ function Todos() {
   const createTodo = () => {
     setTodoName('');
     if (TodoName.trim() !== '')
-      setTodos(prevState => [...prevState, { id: prevState.length, name: TodoName }]);
+      setTodos(prevState => [...prevState, { id: uuidv4(), name: TodoName }]);
     // TodoName && setTodos(prevState => [...prevState, { id: prevState.length, name: TodoName }]);
   };
   const deleteTodo = id => {
     const arr = todos.filter(e => e.id !== id);
     setTodos(arr);
+  };
+  const serachTodo = e => {
+    let value = e.target.value;
+    if (e.key === 'Enter') {
+      if (value.trim() === '') return;
+      const searcharr = todos.filter(e => e.name.includes(value));
+    }
   };
   useEffect(() => {
     try {
@@ -38,6 +46,7 @@ function Todos() {
   return (
     <S.Container>
       <S.Title>To do list</S.Title>
+      <SearchInput placeholder='검색단어' onKeyDown={serachTodo} />
       <CreateItemBox value={TodoName} onChange={setTodoName} createTodoItem={createTodo} />
       <ItemList todos={todos} deleteItem={deleteTodo} />
     </S.Container>
